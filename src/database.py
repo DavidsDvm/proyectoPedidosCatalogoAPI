d5da6f402cb687ce52a9e18f5e31fd62b5060413
DavidsDvm <davids10.dvm@gmail.com>
2021-11-17 22:48:55 -0500

    change the route of the db 4 production

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv('CONNECTION_STRING', 'postgresql://postgres:password@localhost/mintic2')
+SQLALCHEMY_DATABASE_URL = os.getenv('CONNECTION_STRING', 'postgresql://wsjcdinaxlylzy:fa53f75b33ccbf0c6b04e6e9902f6edddf21ded89b08d6c6d2c01c9d9e0e5c68@ec2-3-209-38-221.compute-1.amazonaws.com:5432/dfdeoik7h3qvuf')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
