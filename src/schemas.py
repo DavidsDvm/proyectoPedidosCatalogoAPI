720f3d8c37520d12889a3b09d66c7082fb5e759e
DavidsDvm <davids10.dvm@gmail.com>
2021-11-24 23:44:49 -0500

    modifications on the previus schema

--- src/schemas.py
+++ src/schemas.py
@@ -2,6 +2,24 @@ from typing import Optional
 from pydantic import BaseModel
 
 class CreateUserRequest(BaseModel):
+    id: int
+    identification: str
     name: str
+    address: str
+    cellPhone: str
     email: str
     password: str
+    zone: str
+    type: str
+
+class CreateCookwareRequest(BaseModel):
+    reference: str
+    brand: str
+    category: str
+    materiales: str
+    dimensiones: str
+    description: str
+    availability: bool
+    price: int
+    quantity: int
+    photography: str
